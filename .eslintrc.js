57578a353640415954af72898a5a31f8251694d4
Liam T <liam.t10@icloud.com>
2019-12-22 21:56:44 +0000

    added and configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'plugins': [
+    'jsx-a11y',
+  ],
+  'extends': [
+    'react-app',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  'rules': {}
+}
