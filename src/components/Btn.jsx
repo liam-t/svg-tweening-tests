fd6579de95581f697db7072f03c60b458cc02251
Liam T <liam.t10@icloud.com>
2019-12-27 17:07:17 +0000

    wip on multiple paths to single path morph

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -2,45 +2,58 @@ import React from 'react';
 import styled from 'styled-components';
 import posed from 'react-pose';
 import { tween } from 'popmotion';
-import { interpolate } from 'flubber';
+import {
+  // interpolate,
+  separate,
+  combine,
+} from 'flubber';
 // import PT from 'proptypes';
 
 const propTypes = {};
 const defaultProps = {};
 
 
-const playPath = 'M84.2,43.6L24.4,3.8c-5.1-3.4-12,0.3-12,6.4v79.5c0,6.2,6.9,9.8,12,6.4l59.8-39.8C88.7,53.4,88.7,46.6,84.2,43.6z';
-const stopPath = 'M89.8,2.5H10.2C6,2.5,2.5,6,2.5,10.2v79.6c0,4.3,3.5,7.7,7.7,7.7h79.6c4.3,0,7.7-3.5,7.7-7.7V10.2C97.5,6,94,2.5,89.8,2.5    z';
-const pausedPath = 'M 86.7,2.5 H 64.4 c -2.1,0 -3.9,1.7 -3.9,3.9 v 87.3 c 0,2.1 1.7,3.9 3.9,3.9 h 22.2 c 2.1,0 3.9,-1.7 3.9,-3.9 V 6.4 c 0,-2.2 -1.7,-3.9 -3.8,-3.9 z m -51.1,0 H 13.3 c -2.1,0 -3.9,1.7 -3.9,3.9 v 87.3 c 0,2.1 1.7,3.9 3.9,3.9 h 22.2 c 2.1,0 3.9,-1.7 3.9,-3.9 V 6.4 c 0,-2.2 -1.7,-3.9 -3.8,-3.9 z';
+const playPath = 'M84.2,43.6L24.4,3.8c-5.1-3.4-12,0.3-12,6.4v79.5c0,6.2,6.9,9.8,12,6.4l59.8-39.8C88.7,53.4,88.7,46.6,84.2,43.6 z';
+const stopPath = 'M89.8,2.5H10.2C6,2.5,2.5,6,2.5,10.2v79.6c0,4.3,3.5,7.7,7.7,7.7h79.6c4.3,0,7.7-3.5,7.7-7.7V10.2C97.5,6,94,2.5,89.8,2.5 z';
+const pausedPathElements = [
+  'M 86.7,2.5 H 64.4 c -2.1,0 -3.9,1.7 -3.9,3.9 v 87.3 c 0,2.1 1.7,3.9 3.9,3.9 h 22.2 c 2.1,0 3.9,-1.7 3.9,-3.9 V 6.4 c 0,-2.2 -1.7,-3.9 -3.8,-3.9 z',
+  'm -51.1,0 H 13.3 c -2.1,0 -3.9,1.7 -3.9,3.9 v 87.3 c 0,2.1 1.7,3.9 3.9,3.9 h 22.2 c 2.1,0 3.9,-1.7 3.9,-3.9 V 6.4 c 0,-2.2 -1.7,-3.9 -3.8,-3.9 z',
+];
+const pausedPath = pausedPathElements.join(' ');
 
-const transitionOptions = {
-  maxSegmentLength: 2,
+const transition = ({ from, to }) => {
+  const parsedValFromTo = (fromTo) => {
+    if (typeof fromTo !== 'string') fromTo.join(' ');
+    const splitOnZ = fromTo.split('z');
+    if (splitOnZ.length > 2) {
+      return splitOnZ
+        .filter((item) => item)
+        .map((item) => `${item.trim()} z`);
+    }
+    return fromTo;
+  };
+  const parsedFrom = parsedValFromTo(from);
+  const parsedTo = parsedValFromTo(to);
+  const transitionOptions = {
+    maxSegmentLength: 2,
+    single: true,
+  };
+  const args = [parsedFrom, parsedTo, transitionOptions];
+  const interpolator = parsedTo.length > 0
+    ? separate(...args)
+    : combine(...args);
+
+  return tween({
+    from: 0,
+    to: 1,
+    duration: 600,
+  }).pipe(interpolator);
 };
-const transition = ({ from, to }) => tween({
-  from: 0,
-  to: 1,
-  duration: 300,
-}).pipe(interpolate(from, to, transitionOptions));
-
-const PosedIcon = posed.path({
-  playing: {
-    transition,
-    d: playPath,
-  },
-  stopped: {
-    transition,
-    d: stopPath,
-  },
-  paused: {
-    transition,
-    d: pausedPath,
-  },
-});
 
 
 const Btn = () => {
-  const [isActive, setIsActive] = React.useState(false);
-  const activePose = isActive ? 'playing' : 'stopped';
+  const [isActive, setIsActive] = React.useState(true);
+  const activePose = isActive ? 'playing' : 'paused';
   const activeColor = isActive ? '#327772' : '#242c56';
   const clickHandle = () => setIsActive(!isActive);
 
@@ -56,11 +69,30 @@ Btn.propTypes = propTypes;
 Btn.defaultProps = defaultProps;
 export default Btn;
 
-const BtnOuter = styled.button``;
+const BtnOuter = styled.button`
+  background-color: transparent;
+  outline: none;
+  border: none;
+`;
 const Svg = styled.svg`
   width: 100px;
   height: 100px;
 `;
+
+const PosedIcon = posed.path({
+  playing: {
+    transition,
+    d: playPath,
+  },
+  stopped: {
+    transition,
+    d: stopPath,
+  },
+  paused: {
+    transition,
+    d: pausedPath,
+  },
+});
 const Icon = styled(PosedIcon)`
   transition: fill 0.3s;
 `;
